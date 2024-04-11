6934d9cf52133bbf043c251aa7b9ba83ffd0d30c
Mike RR <michaelandresrr@gmail.com>
2024-04-11 11:12:17 -0500

    nueva asignacion

---
+++ CUENTA 3/imagenes.py
@@ -0,0 +1,15 @@
+Seleccionar por atributos uno o mas municipios saber cuantas imagenes municipios
+sacar prom area imagenes
+sumar todas las areas y factor 70%
+ese num multiplar prom imagenes y factor
+saber cuanto se paga
+
+reporte
+
+cuantas imagenes netas caben y ya, poner dinamico el factor de traslap
+
+
+no tiene resolucion con la que se rija 
+
+sacar el area total de area de las imagenes, sacar overlap 70%
+sacar promedio de todas las imagenes 
