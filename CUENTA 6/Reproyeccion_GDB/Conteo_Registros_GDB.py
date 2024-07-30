1fc26e1a11d088950cfbcd85fc71f2b2ec5f435e
Mike RR <michaelandresrr@gmail.com>
2024-07-30 13:46:27 -0500

    E

--- CUENTA 6/Reproyeccion_GDB/Conteo_Registros_GDB.py
+++ CUENTA 6/Reproyeccion_GDB/Conteo_Registros_GDB.py
@@ -6,6 +6,16 @@ arcpy.env.overwriteOutput = True
 inGDB = arcpy.GetParameterAsText(0)  
 xml = arcpy.GetParameterAsText(1)
 outXML = arcpy.GetParameterAsText(2)
+repair = arcpy.GetParameterAsText(3)
+
+def repair_geometry(feature_classes):
+    for fc in feature_classes:
+        try:
+            arcpy.AddMessage(f"Reparando geometría para: {fc}")
+            arcpy.management.RepairGeometry(fc)
+            arcpy.AddMessage(f"Geometría reparada para: {fc}")
+        except arcpy.ExecuteError:
+            arcpy.AddError(f"Error al reparar geometría para {fc}")
 
 def contar_registros(inGDB, outXML):
 
@@ -13,8 +23,8 @@ def contar_registros(inGDB, outXML):
     arcpy.AddMessage(f"--------------------------------------------------------")
     arcpy.env.workspace = inGDB
     
-    # Listar todos los datasets en la geodatabase
-    datasets = arcpy.ListDatasets(feature_type='feature') or [""]  # Manejo de None
+    # Listar todos los datasets en la geodatabase y ordenarlos alfabéticamente
+    datasets = sorted(arcpy.ListDatasets(feature_type='feature') or [""])  # Manejo de None
     
     # Diccionario para almacenar los resultados
     resultados = {}
@@ -24,13 +34,21 @@ def contar_registros(inGDB, outXML):
         resultados[dataset] = {}
         total_por_dataset = 0  # Contador para el total por dataset
         
-        # Listar todas las clases de entidad dentro de cada dataset
-        feature_classes = arcpy.ListFeatureClasses(feature_dataset=dataset)
+        # Listar todas las clases de entidad dentro de cada dataset y ordenarlas alfabéticamente
+        feature_classes = sorted(arcpy.ListFeatureClasses(feature_dataset=dataset))
+        
+        # Reparar geometría antes de contar los registros
+        if repair == "true":
+            full_fc_paths = [os.path.join(inGDB, dataset, fc) for fc in feature_classes]
+            repair_geometry(full_fc_paths)
+        else:
+            pass
         
         for fc in feature_classes:
             try:
                 # Contar los registros en cada clase de entidad
-                count = int(arcpy.management.GetCount(os.path.join(inGDB, dataset, fc)).getOutput(0))
+                fc_path = os.path.join(inGDB, dataset, fc)
+                count = int(arcpy.management.GetCount(fc_path).getOutput(0))
                 resultados[dataset][fc] = count
                 total_por_dataset += count  # Sumar al total por dataset
             except arcpy.ExecuteError:
@@ -42,7 +60,6 @@ def contar_registros(inGDB, outXML):
         arcpy.AddMessage(f"Total de registros en el dataset {dataset}: {total_por_dataset}")
 
     # Imprimir el total de registros en la geodatabase
-    
     arcpy.AddMessage(f"Total de registros en la geodatabase: {total_registros}")
     arcpy.AddMessage(f"--------------------------------------------------------")
     
