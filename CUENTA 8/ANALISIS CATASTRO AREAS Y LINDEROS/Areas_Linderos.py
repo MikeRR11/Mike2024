14b8fbb9b920bf51b3dd165832e6c69e78391cc3
Mike RR <michaelandresrr@gmail.com>
2024-08-30 11:56:09 -0500

    Co-authored-by: yariquevedo <yariquevedo@users.noreply.github.com>

--- CUENTA 8/ANALISIS CATASTRO AREAS Y LINDEROS/Areas_Linderos.py
+++ CUENTA 8/ANALISIS CATASTRO AREAS Y LINDEROS/Areas_Linderos.py
@@ -2,7 +2,7 @@ import arcpy
 import os
 
 # Obtener las rutas de la GDB y la carpeta de salida desde las entradas del usuario
-shp = arcpy.GetParameterAsText(0)
+shapefile = arcpy.GetParameterAsText(0)
 xlx = arcpy.GetParameterAsText(1)
 Ruta_Salida = arcpy.GetParameterAsText(2)
 zona = arcpy.GetParameterAsText(3)
@@ -12,7 +12,37 @@ arcpy.env.overwriteOutput = True
 
 arcpy.AddMessage("INICIANDO PROCESO ...............")
 arcpy.AddMessage("  ")
-
 arcpy.AddMessage(f"Cálculando parametros para zona {zona}.")
+
+#1 crear copia del shp original
+
+# Nombre de salida para el shapefile de intersección
+nombre_salida = f"Tolerancia_{os.path.basename(shapefile)[:10]}.shp"
+ruta_salida = os.path.join(Ruta_Salida, nombre_salida)
+shp = arcpy.management.CopyFeatures(shapefile, ruta_salida)
+arcpy.management.RepairGeometry(in_features = shp, delete_null="DELETE_NULL", validation_method="OGC")
+
+#2 Añadir campo para el parametro de % de tolerancia y evaluacion del mismo (OK y NO OK)
+
+arcpy.AddField_management(shp, 'Tolerancia %', 'double')
+arcpy.AddField_management(shp, 'Tolerancia OK', 'text')
+
+#3 Realizar join
+
+arcpy.management.AddJoin(
+    in_layer_or_view = shp,
+    in_field = "CODIGO",
+    join_table = xlx,
+    join_field = "NUMERO_PREDIAL",
+    join_type = "KEEP_ALL",
+    index_join_fields = "NO_INDEX_JOIN_FIELDS",
+    rebuild_index = "NO_REBUILD_INDEX"
+)
+
+#4 Calcular campo de % de tolerancia
+
+tolerancia = arcpy.management.CalculateField(in_table, field, expression, {expression_type}, {code_block}, {field_type}, {enforce_domains})
+#5 Calcular evaluacion de tolerancia respecto a tabla IGAC
+
 
 
--- CUENTA 8/SUPERPOSICIÓN DE POLIGONOS/Superposición poligonos.py
+++ CUENTA 8/SUPERPOSICIÓN DE POLIGONOS/Superposición poligonos.py
@@ -10,6 +10,7 @@ arcpy.env.overwriteOutput = True
 
 arcpy.AddMessage("INICIANDO PROCESO ...............")
 arcpy.AddMessage("  ")
+
 # Crear lista de feature classes de tipo polígono, excluyendo anotaciones
 fc_list = []
 for dirpath, dirnames, filenames in arcpy.da.Walk(GDB, datatype="FeatureClass", type="Polygon"):
@@ -18,16 +19,33 @@ for dirpath, dirnames, filenames in arcpy.da.Walk(GDB, datatype="FeatureClass", 
         if desc.featureType not in ["Annotation", "CoverageAnnotation"]:
             fc_list.append(os.path.join(dirpath, filename))
 
-
 # Total de feature classes
 total_fc = len(fc_list)
 
-# Procesamiento de intersecciones
+# Procesamiento de intersecciones y relaciones espaciales adicionales
 for i, fc1 in enumerate(fc_list):
-    # Calcular el progreso como un porcentaje
     progreso = int((i / float(total_fc - 1)) * 100)
     arcpy.SetProgressorLabel(f"Analizando: {os.path.basename(fc1)}")
     arcpy.AddMessage(f"Progreso: {progreso}% - Analizando: {os.path.basename(fc1)}")
+    
+    # Verificación entre los polígonos de la misma capa
+    oids_procesados = set()  # Conjunto para rastrear los pares de OID procesados
+    with arcpy.da.SearchCursor(fc1, ["OID@", "SHAPE@"]) as cursor1:
+        for row1 in cursor1:
+            oid1, geom1 = row1
+            with arcpy.da.SearchCursor(fc1, ["OID@", "SHAPE@"]) as cursor2:
+                for row2 in cursor2:
+                    oid2, geom2 = row2
+                    if oid1 != oid2 and (oid2, oid1) not in oids_procesados:  # Evita la duplicación
+                        if geom1.touches(geom2) or geom1.within(geom2) or geom1.overlaps(geom2):
+                            nombre_salida = f"Rel_{os.path.basename(fc1)[:10]}_{oid1}_{oid2}.shp"
+                            ruta_salida = os.path.join(Ruta_Salida, nombre_salida)
+                            arcpy.management.CopyFeatures([geom1, geom2], ruta_salida)
+                            arcpy.AddMessage(f"   - Relación espacial encontrada entre OID {oid1} y OID {oid2}")
+                            
+                            # Marcar este par de OIDs como procesados
+                            oids_procesados.add((oid1, oid2))
+    
     for j in range(i + 1, len(fc_list)):
         fc2 = fc_list[j]
         arcpy.AddMessage(f" - Comparando con: {os.path.basename(fc2)}")
