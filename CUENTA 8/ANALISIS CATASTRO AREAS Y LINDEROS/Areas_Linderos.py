7d4e80c76132a317fc3ad39cacb477834208c6ce
Mike RR <michaelandresrr@gmail.com>
2024-09-02 11:06:43 -0500

    ss

--- CUENTA 8/ANALISIS CATASTRO AREAS Y LINDEROS/Areas_Linderos.py
+++ CUENTA 8/ANALISIS CATASTRO AREAS Y LINDEROS/Areas_Linderos.py
@@ -34,71 +34,64 @@ arcpy.management.JoinField(
     join_field="NUMERO_PRE",
     fields=["AREATERR"])
 
-# 5. Calcular el campo de % de tolerancia (VALOR ABSOLUTO) en el shapefile final
-arcpy.AddMessage("Calculando campos...")
 
-# Agregar el campo Tol_P como tipo DOUBLE (puede ser FLOAT también)
+# Agregar el campo Tol_P como tipo DOUBLE
 arcpy.management.AddField(in_table=shp_copy, field_name="Tol_P", field_type="DOUBLE")
 
-#Calcular campo Tol_P
+# Calcular el campo Tol_P
 arcpy.management.CalculateField(
     in_table=shp_copy,
     field="Tol_P",
-    expression="round(abs((!Shape_Area! - !AREATERR!) / !Shape_Area! * 100),3)",
+    expression="round(abs((!Shape_Area! - !AREATERR!) / !Shape_Area! * 100), 3)",
     expression_type="PYTHON3"
 )
 
 # Agregar el campo Tol_OK como tipo texto
 arcpy.management.AddField(in_table=shp_copy, field_name="Tol_OK", field_type="TEXT")
 
+# Determinar el bloque de código según la zona
+if zona == 'Urbano':
+    # Definir el código de la función como un bloque de texto
+    code_block = """
+def validar_tolerancia(area, tolerancia):
+    if area <= 80:
+        max_tolerancia = 7
+    elif 80 < area <= 250:
+        max_tolerancia = 6
+    elif 250 < area <= 500:
+        max_tolerancia = 4
+    else:
+        max_tolerancia = 3
+
+    return "OK" if tolerancia <= max_tolerancia else "NO OK"
+"""
+else:
+    code_block = """
+def validar_tolerancia(area, tolerancia):
+    if area <= 2000:
+        max_tolerancia = 10
+    elif 2000 < area <= 10000:
+        max_tolerancia = 9
+    elif 10000 < area <= 100000:
+        max_tolerancia = 7
+    elif 100000 < area <= 500000:
+        max_tolerancia = 4
+    else:
+        max_tolerancia = 2
+
+    return "OK" if tolerancia <= max_tolerancia else "NO OK"
+"""
+
 # Definir la expresión de cálculo para el campo Tol_OK
 expression = "validar_tolerancia(!Shape_Area!, !Tol_P!)"
 
-#6. Definir si el porcentaje de tolerancia cumple o no cumple segun áreas
-if zona == 'Urbano':
-# Definir el código de la función como un bloque de texto
-    code_block = """
-    def validar_tolerancia(area, tolerancia):
-        if area <= 80:
-            max_tolerancia = 7
-        elif 80 < area <= 250:
-            max_tolerancia = 6
-        elif 250 < area <= 500:
-            max_tolerancia = 4
-        else:
-            max_tolerancia = 3
-
-        return "OK" if tolerancia <= max_tolerancia else "NO OK"
-    """
-    # Calcular el campo Tol_OK utilizando la expresión y el código de la función
-    arcpy.management.CalculateField(
-        in_table=shp_copy,
-        field="Tol_OK",
-        expression=expression,
-        expression_type="PYTHON3",
-        code_block=code_block
-        )
-else:
-    code_block = """
-    def validar_tolerancia(area, tolerancia):
-        if area <= 2000:
-            max_tolerancia = 10
-        elif 2000 < area <= 250:
-            max_tolerancia = 6
-        elif 250 < area <= 500:
-            max_tolerancia = 4
-        else:
-            max_tolerancia = 3
-
-        return "OK" if tolerancia <= max_tolerancia else "NO OK"
-    """
-    # Calcular el campo Tol_OK utilizando la expresión y el código de la función
-    arcpy.management.CalculateField(
-        in_table=shp_copy,
-        field="Tol_OK",
-        expression=expression,
-        expression_type="PYTHON3",
-        code_block=code_block
+# Calcular el campo Tol_OK utilizando la expresión y el código de la función
+arcpy.management.CalculateField(
+    in_table=shp_copy,
+    field="Tol_OK",
+    expression=expression,
+    expression_type="PYTHON3",
+    code_block=code_block
 )
 
 arcpy.AddMessage("Proceso completado.")
