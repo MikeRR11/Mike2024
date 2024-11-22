a5e45a458a4baeb7de1f910a6aff3bdde3f38de3
Mike RR <michaelandresrr@gmail.com>
2024-11-22 09:40:01 -0500

    Co-authored-by: yariquevedo <yariquevedo@users.noreply.github.com>

--- CUENTA 11/TABLAS ANOTACIONES ARCMAP.py
+++ CUENTA 11/TABLAS ANOTACIONES ARCMAP.py
@@ -1,1 +1,12 @@
 import arcpy 
+import os
+
+#Feature clas
+
+anotacion = arcpy.GetParameterAsText(0)
+Ruta_Salida = arcpy.GetParameterAsText(1)
+
+arcpy.AddMessage("INICIANDO PROCESO")
+
+#with arcpy.SearchCursor(anotacion, [])
+
